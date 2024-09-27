e0135dd890e1b62e5a4055f15484295ba357202d
Hlayi2 <katlegokatenguyaza01@gmail.com>
2024-09-27 11:32:54 +0200

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp13
+
+{
+    //Creating a class
+    class MilkSong
+    {
+        //Method to sing the MilkSong
+        public void sing()
+        {
+            //Loop statement
+            for (int bottles = 99; bottles > 0; bottles--)
+            {
+                if (bottles > 1)
+                {
+                    Console.WriteLine($"{bottles} bottles of milk on the wall,{bottles} bottles of milk.Take one down and pass it around,\n");
+                }
+                else
+                {
+                    Console.WriteLine($"1 bottle of milk on the wall, 1 bottle of the milk.Take one down and pass it around\n");
+                }
+            }
+        }
+    }
+    // Main program class
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an instance of MiklSong
+
+            MilkSong song = new MilkSong();
+            song.sing();
+        }
+    }
+}
+
+
+
+
+
+
+
